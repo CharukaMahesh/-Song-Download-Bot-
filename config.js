2abc25b334f0cbc8518299261ff08a986f1eca38
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-09-16 07:59:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_MSG: process.env.ALIVE_MSG || "හායී කොහොමද..👋",
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/CharukaMahesh/-Song-Download-Bot-/main/img/20240909_203419.png",
     
 
 };
